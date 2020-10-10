edc1393b5cdd2c39e9d44792d02c8a36df231960
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2020-10-10 17:57:07 +0000

    Update gemspec

--- lib/validates_unchangeable.rb
+++ lib/validates_unchangeable.rb
@@ -1,6 +1,8 @@
-require "validates_unchangeable/version"
-require "validates_unchangeable/active_model"
-I18n.load_path += Dir[File.dirname(__FILE__) + "../config/locale/*.yml"]
+# frozen_string_literal: true
+
+require 'validates_unchangeable/version'
+require 'validates_unchangeable/active_model'
+I18n.load_path += Dir[File.dirname(__FILE__) + '../config/locale/*.yml']
 
 module ValidatesUnchangeable
 end
