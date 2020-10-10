26ffa5c3e06022aafde262530963ad35147c9f8e
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2020-10-10 17:59:27 +0000

    Lint

--- lib/validates_unchangeable/active_model.rb
+++ lib/validates_unchangeable/active_model.rb
@@ -4,9 +4,9 @@ module ActiveModel
   module Validations
     class UnchangeableValidator < ActiveModel::EachValidator
       def validate_each(record, attribute, _value)
-        if record.public_send("#{attribute}_changed?") && record.public_send("#{attribute}_was").present?
-          record.errors.add(attribute, :unchangeable)
-        end
+        return if !record.public_send("#{attribute}_changed?") || record.public_send("#{attribute}_was").nil?
+
+        record.errors.add(attribute, :unchangeable)
       end
     end
   end
