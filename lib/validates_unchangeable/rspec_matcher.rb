8f0924a72d8501e54fc443de0b893dfd4840db84
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2020-10-11 08:42:54 +0000

    Fixed matcher

--- lib/validates_unchangeable/rspec_matcher.rb
+++ lib/validates_unchangeable/rspec_matcher.rb
@@ -4,8 +4,8 @@ RSpec::Matchers.define :validate_unchangeable_of do |attribute|
   match do
     actual = subject.is_a?(Class) ? subject.new : subject
     actual.public_send(:"#{attribute}=", 'qwerty') if actual.public_send(attribute).nil?
-    actual.save(validate: false)
-    actual.public_send(:"#{attribute}=", 'ytrewq')
+    actual.save(validate: false) if actual.new_record? || actual.changes.present?
+    actual.public_send(:"#{attribute}=", nil)
     expect(actual).to be_invalid
     @expected_message ||= I18n.t('errors.messages.unchangeable')
     expect(actual.errors.messages[attribute.to_sym]).to include(@expected_message)
