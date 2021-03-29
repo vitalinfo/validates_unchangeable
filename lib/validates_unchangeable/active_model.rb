85a7f9f72bd85e05056ddea83c6c19cb1f005bbf
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2021-03-29 19:07:13 +0000

    Fixed unchangeable with default value

--- lib/validates_unchangeable/active_model.rb
+++ lib/validates_unchangeable/active_model.rb
@@ -6,9 +6,20 @@ module ActiveModel
   module Validations
     class UnchangeableValidator < ActiveModel::EachValidator
       def validate_each(record, attribute, _value)
-        return if !record.public_send("#{attribute}_changed?") || record.public_send("#{attribute}_was").nil?
+        if !record.public_send("#{attribute}_changed?") ||
+            record.public_send("#{attribute}_was") == default_value_for(record, attribute)
+          return
+        end
 
         record.errors.add(attribute, :unchangeable)
+      end
+
+      private
+
+      def default_value_for(record, attribute)
+        return unless record.class.respond_to?(:column_defaults)
+
+        record.class.column_defaults[attribute.to_s]
       end
     end
   end
--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.shared_examples_for 'unchangeable: validation' do |klass| # rubocop:disable Metrics/BlockLength
+RSpec.shared_examples_for 'unchangeable: validation' do |klass, attribute, value, new_value| # rubocop:disable Metrics/BlockLength
   describe 'when create' do
     context 'without value' do
       let(:object) { klass.new.tap(&:save) }
@@ -9,7 +9,7 @@ RSpec.shared_examples_for 'unchangeable: validation' do |klass| # rubocop:disabl
     end
 
     context 'with value' do
-      let(:object) { klass.create(value: 'qwerty') }
+      let(:object) { klass.create(attribute => value) }
 
       it { expect(object).to be_valid }
     end
@@ -20,17 +20,17 @@ RSpec.shared_examples_for 'unchangeable: validation' do |klass| # rubocop:disabl
       let(:object) { klass.create }
 
       before do
-        object.value = 'qwerty'
+        object.public_send("#{attribute}=", value)
       end
 
       it { expect(object).to be_valid }
     end
 
-    context 'without value' do
-      let(:object) { klass.create(value: 'qwerty') }
+    context 'with value' do
+      let(:object) { klass.create(attribute => value) }
 
       before do
-        object.value = 'ytrewq'
+        object.public_send("#{attribute}=", new_value)
       end
 
       it { expect(object).to be_invalid }
